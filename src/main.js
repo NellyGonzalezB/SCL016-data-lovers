299bca2dd10a67198ac5f29d79d34ec740cd4117
Claudia Osses <claudia.osses.mundaca@gmail.com>
2021-03-17 09:14:29 -0300

    modificacion html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+const button = () => {
+    document.querySelector('.welcome').style.display = "none";
+    document.querySelector('.second-page').style.display = "block";
+  }
+
+
+  document.querySelector(".pokeball").addEventListener("click", button);
