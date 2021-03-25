a883c415da9f013c0a571dbe965a9032127d38a7
Claudia Osses <claudia.osses.mundaca@gmail.com>
2021-03-25 11:13:42 -0300

    modificaciones js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,38 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(example, data);
 
 const firstButton = () => {
-  document.querySelector('.first-page').style.display = "none";
-  document.querySelector('.second-page').style.display = "block";
+    document.querySelector('.first-page').style.display = "none";
+    document.querySelector('.second-page').style.display = "block";
 }
 
 const kantoRegion = () => {
-  document.querySelector('.second-page').style.display = "none";
-  document.querySelector('.kanto-location').style.display = "block";
+    const pokemon = data.pokemon.filter(data => data.generation.name == "kanto");
+        console.log(pokemon);
+    const containerK = document.querySelector(".kanto-results");
+    const pokeMap = pokemon.map((data) => {
+        containerK.innerHTML += `
+        <li class="poke-image">
+            <figure>
+                <img class="pokemon-image" src=${data.img}>
+                <div class="pokemon-name">${data.name}</div>
+             </figure>
+        </li>
+`;
+    } )
+
+    document.querySelector('.second-page').style.display = "none";
+    document.querySelector('.kanto-location').style.display = "block";
 }
 
 const johtoRegion = () => {
-  document.querySelector('.second-page').style.display = "none";
-  document.querySelector('.johto-location').style.display = "block";
+    const pokemonJohto = data.pokemon.filter(data => data.generation.name == "johto");
+        console.log(pokemonJohto);
+    document.querySelector('.second-page').style.display = "none";
+    document.querySelector('.johto-location').style.display = "block";
 }
 
 document.querySelector(".poke-button").addEventListener("click", firstButton);
